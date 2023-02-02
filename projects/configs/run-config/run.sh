b1121737ddc18f6180d739b768a7fc1c48c70d3d
icetbr <icetbr@yahoo.com.br>
2023-02-02 14:22:30 -0300

    cleaned run configs

--- projects/configs/run-config/run.sh
+++ projects/configs/run-config/run.sh
@@ -1,10 +1,17 @@
 #!/bin/bash
 
-# echo $PWD
-# source ../@icetbr/run-config/common.sh
-# source ./common.sh
+# -e stops on error
+# -u stops on unset variable
+# -o pipefail stops if any part of the pipeline fails
+set -euo pipefail
+
 RUN_PATH=$(dirname $0)
-# echo $RUN_PATH
+
+set -a && . $RUN_PATH/../../.env 2> /dev/null && set +a    # load global .env
+set -a && . .env 2> /dev/null && set +a                    # load local .env
+
+PATH=$PATH:$RUN_PATH                                       # enable calls like `mocha` instead of `node_modules/.bin/mocha`
+
 source $RUN_PATH/../@icetbr/run-config/common.sh
 
 "$@"
