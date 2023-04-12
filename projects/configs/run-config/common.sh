db9fc52659619682183d37b1ae91f8cfd26852a6
icetbr <icetbr@yahoo.com.br>
2023-04-11 21:59:48 -0300

    - fixex chromeId - more adjustments do manifestV3

--- projects/configs/run-config/common.sh
+++ projects/configs/run-config/common.sh
@@ -6,6 +6,8 @@ iconParams="{
     sizes: [16, 32, 48, 96, 128]
 }"
 
+chromeId=$(grep addonIds manifest.json | grep -Eo '/\w*'| cut -c2-)
+
 nodeSh     () { node -e "import('@icetbr/utils/$1').then(m => m.$2(${3:- }))"   ;} # Usage: nodeSh fileName functionName params
 # nodeSh       () { node -e "import('@icetbr/utils/$1').then(m => console.log(m.$1(${2:- })))"  ;}
 # _nodeSh2      () { node -e "import('@icetbr/utils/$1').then(m => m.$2(${3:- }))"   ;}
@@ -26,11 +28,11 @@ firefoxReadme          () { nodeSh readme openFirefoxReadme                   ;}
 chromeReadme           () { nodeSh readme openChromeReadme                    ;}
 
 uploadFf         () { web-ext sign --channel=listed --api-key=$FIREFOX_KEY --api-secret=$FIREFOX_SECRET                                                                  ;}
-uploadChrome     () { chrome-webstore-upload upload --source dist --extension-id $CHROME_ID --client-id $CHROME_KEY --client-secret $CHROME_SECRET --refresh-token $CHROME_REFRESH_TOKEN ;}
+uploadChrome     () { chrome-webstore-upload upload --source dist --extension-id $chromeId --client-id $CHROME_KEY --client-secret $CHROME_SECRET --refresh-token $CHROME_REFRESH_TOKEN ;}
 zipSrc           () { cd dist && zip -r -FS ../webext * && cd ..                                ;}
 lint             () { web-ext lint                                                              ;} # lint is mandatory!
-copyFilesToDist  () { cp -R manifest.json media/icons dist                                      ;}
-adjustManifestV3 () { sed -i '0,/2/s//3/' dist/manifest.json && sed -i '18,24d' dist/manifest.json  ;}
+copyFilesToDist  () { cp -R manifest.json src/options.html src/options.js src/state.js media/icons dist 2>/dev/null                                     ;}
+adjustManifestV3 () { sed -i '0,/2/s//3/' dist/manifest.json && sed -i '18,24d' dist/manifest.json && sed -i 's/browser_action/action/g' dist/manifest.json && sed -i 's/browser./chrome./g' dist/content.js && sed -i 's/browser./chrome./g' dist/options.js;}
 bundle           () { rollup --config node:@icetbr/rollup-config-webext                      ;}
 
 ## PUBLISH
